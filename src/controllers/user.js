09f6d0eb94ab7b7263fc0ce9acba9d61ca8c4645
reyesbho <reyesbho@gmail.com>
2025-07-01 13:41:19 -0600

    modified for commit

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -34,7 +34,7 @@ export class UserController {
                 .cookie('access_token', access_token,{
                     httpOnly:true,
                     secure:process.env.NODE_ENV == 'production',
-                    sameSite:'strict',
+                    sameSite:'lax',
                     maxAge: 1000 * 60 * 60
                 } )
                 .json({token: access_token});
