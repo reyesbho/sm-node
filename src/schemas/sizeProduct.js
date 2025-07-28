035275132dd665272a21f6d3e8b272b7b3132e44
reyesbho <reyesbho@gmail.com>
2025-07-28 16:21:02 -0600

    reduce size requiered

--- src/schemas/sizeProduct.js
+++ src/schemas/sizeProduct.js
@@ -1,7 +1,7 @@
 import {boolean, object, string} from 'zod';
 
 const sizeProduct = object({
-    descripcion : string().min(5, 'Min character length is 5'),
+    descripcion : string().min(4, 'Min character length is 4'),
     estatus: boolean().default(true),
     tags: string().array().optional()
 });
