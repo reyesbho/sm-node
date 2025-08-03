e7646fbca526d2d76d8f83be9a77b94a97bae4ed
reyesbho <reyesbho@gmail.com>
2025-08-02 19:48:20 -0600

    fix export

--- src/test/routes/Size.test.ts
+++ src/test/routes/Size.test.ts
@@ -1,14 +1,15 @@
-import { app, server } from '../../../server';
+
 import request from 'supertest';
+import server from '../../../server.js';
 
 afterAll(() => {
-  server.close(); 
+  server.server.close(); 
 });
 
 let authCookie: string;
 describe("Catalogs sizes", () => {
     it("should authtenticate the request", async () => {
-        const response = await request(app).post("/user/login")
+        const response = await request(server.app).post("/user/login")
         .send(
             {
                 "email":"test@test.com",
@@ -23,21 +24,21 @@ describe("Catalogs sizes", () => {
     });
 
     it("should return a list of sizes", async () => {
-        const response = await request(app).get("/api/sizes")
+        const response = await request(server.app).get("/api/sizes")
         .set('Cookie', `access_token=${authCookie}`);
         expect(response.status).toBe(200);
         expect(response.body).toBeInstanceOf(Array);
     });
     
     it("should return a size by ID", async () => {
-        const response = await request(app).get("/api/sizes/F5kvF254DPYGgwv6yuj7")
+        const response = await request(server.app).get("/api/sizes/F5kvF254DPYGgwv6yuj7")
         .set('Cookie', `access_token=${authCookie}`);
         expect(response.status).toBe(200);
         expect(response.body).toHaveProperty('id', 'F5kvF254DPYGgwv6yuj7');
     });
     
     it("should return 404 for non-existing size", async () => {
-        const response = await request(app).get("/api/sizes/9999")
+        const response = await request(server.app).get("/api/sizes/9999")
         .set('Cookie', `access_token=${authCookie}`);
         expect(response.status).toBe(404);
     });
@@ -48,7 +49,7 @@ describe("Catalogs sizes", () => {
             descripcion: "TestSize",
             tags: ["test", "test"],
         };
-        const response = await request(app).post("/api/sizes")
+        const response = await request(server.app).post("/api/sizes")
         .set('Cookie', `access_token=${authCookie}`)
         .send(newSize);
         expect(response.status).toBe(201);
@@ -63,7 +64,7 @@ describe("Catalogs sizes", () => {
             descripcion: "TestSize",
             tags: ["test", "test", "updated"],
         };
-        const response = await request(app).patch(`/api/sizes/${sizeId}`)
+        const response = await request(server.app).patch(`/api/sizes/${sizeId}`)
         .set('Cookie', `access_token=${authCookie}`)
         .send(updatedSize);
         expect(response.status).toBe(200);
@@ -72,7 +73,7 @@ describe("Catalogs sizes", () => {
     });
 
     it("should update status an existing size", async () => {
-        const response = await request(app).put(`/api/sizes/${sizeId}`)
+        const response = await request(server.app).put(`/api/sizes/${sizeId}`)
         .set('Cookie', `access_token=${authCookie}`);
         expect(response.status).toBe(204);
         expect(response.body).toHaveProperty('id', sizeId);
