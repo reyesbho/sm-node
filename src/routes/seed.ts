62ab89dca02fe15b296987dd22dc2b1af46198f3
reyesbho <reyesbho@gmail.com>
2026-02-04 18:24:58 -0600

    fix for build

--- src/routes/seed.ts
+++ src/routes/seed.ts
@@ -2,11 +2,12 @@ import { Router } from "express"
 import { PedidoModel } from "../models/firebase/Pedido.js";
 import { ProductModel } from "../models/firebase/Product.js";
 import { SeedController } from "../controllers/seed.js";
+import { CategoryModel } from "../models/firebase/Category.js";
 
-export const createSeedRouter = (pedidoModel: PedidoModel, productModel: ProductModel) => {
+export const createSeedRouter = (pedidoModel: PedidoModel, productModel: ProductModel, categoryModel: CategoryModel) => {
     const seedRouter = Router();
     const seedController = new SeedController({
-        pedidoModel, productModel
+        pedidoModel, productModel,categoryModel
     });
 
     seedRouter.post('/', seedController.seed);
